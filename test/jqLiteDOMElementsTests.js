189774e4f1888f7bb7f9516bd770f2acbbb4c044
JuanMa Ruiz <ruizjuanma@gmail.com>
2018-07-08 20:28:37 +0200

    Added test to check the lenght of elements retrieved

--- test/jqLiteDOMElementsTests.js
+++ test/jqLiteDOMElementsTests.js
@@ -1,17 +1,21 @@
 describe("DOM Element methods", function() {
-  beforeEach(function () {
-    // add an element to the page to render into
-    let testElement = $('<section></section>');
-    $('body').append(testElement);
-  });
   it('jqLite.qsa should return an array', function () {
-    assert.typeOf(jqLite.qsa('section'), "array", "we have an array");
+    assert.typeOf(jqLite.qsa('aside'), "array", "we have an array");
+  });
+
+  before(function () {
+    // add an element to the page to render into
+    let TEST_ELEMENT = $(`<aside></aside>`);
+    $('body').append(TEST_ELEMENT);
+  });
+  it('jqLite.qsa("aside") should return one element', function () {
+    assert.lengthOf(jqLite.qsa('aside'), 1, "only one element returned");
   });
 
   before(function () {
     // Adds to elements on the page with jQuery
-    let testElement = $('<div class="foo"></div class="foo"><div class="foo"></div class="foo">');
-    $('body').append(testElement);
+    let TEST_ELEMENT = $('<div class="foo"></div><div class="foo"></div>');
+    $('body').append(TEST_ELEMENT);
   });
   it('jqLite.qsa should find 2 foo elements in the DOM', function () {
     assert.lengthOf(jqLite.qsa('.foo'), 2, "The 2 div.foo elements were not found in the html");
