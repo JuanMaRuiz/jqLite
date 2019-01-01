e1546e678b1c6df77fd691845380bad67884e29b
JuanMa Ruiz <ruizjuanma@gmail.com>
2019-01-01 17:15:58 +0100

    Clear test

--- test/jqLiteDOMElementsTests.js
+++ test/jqLiteDOMElementsTests.js
@@ -1,22 +1,24 @@
 describe("DOM Element methods", function() {
-  it('jqLite.qsa should return an array', function () {
+  it('jqLite.qsa method should return an array of elements', function () {
     assert.typeOf(jqLite.qsa('aside'), "array", "we have an array");
   });
 
   before(function () {
     // add an element to the page to render into
-    let TEST_ELEMENT = $(`<aside></aside>`);
+    const TEST_ELEMENT = $(`<aside></aside>`);
     $('body').append(TEST_ELEMENT);
   });
+
   it('jqLite.qsa("aside") should return one element', function () {
     assert.lengthOf(jqLite.qsa('aside'), 1, "only one element returned");
   });
 
   before(function () {
     // Adds to elements on the page with jQuery
-    let TEST_ELEMENT = $('<div class="foo"></div><div class="foo"></div>');
+    const TEST_ELEMENT = $('<div class="foo"></div><div class="foo"></div>');
     $('body').append(TEST_ELEMENT);
   });
+
   it('jqLite.qsa should find 2 foo elements in the DOM', function () {
     assert.lengthOf(jqLite.qsa('.foo'), 2, "The 2 div.foo elements were not found in the html");
   });
--- test/jqLiteGenericTest.js
+++ test/jqLiteGenericTest.js
@@ -1,9 +1,11 @@
 describe("Generic Tests for jqLite Library", function () {
-    it("should be defined on window load", function () {
-        assert.isDefined(window.jqLite, 'jqLite has not been defined')
+    describe('jqLite', () => {
+        it("should be defined on window load", function () {
+            assert.isDefined(window.jqLite, 'jqLite has not been defined')
+        });
+        it('VERSION should return the correct version', function() {
+          assert.equal(jqLite.version, '0.1.0', "jqLite.getVersion() provides the incorrect version");
+        });
     });
 
-    it('jqLite.VERSION should return the correct version', function() {
-      assert.equal(jqLite.version, '0.1.0', "jqLite.getVersion() provides the incorrect version");
-    });
 });
