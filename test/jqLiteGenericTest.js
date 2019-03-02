154525a796fd7efba5d4ae722806b73ec2becabb
JuanMa Ruiz <ruizjuanma@gmail.com>
2019-03-02 18:04:56 +0100

    - Updated package-lock - Updated jqLite.version tests

--- test/jqLiteGenericTest.js
+++ test/jqLiteGenericTest.js
@@ -3,8 +3,13 @@ describe("Generic Tests for jqLite Library", function () {
     it("should be defined on window load", function () {
       assert.isDefined(window.jqLite, 'jqLite has not been defined')
     });
-    it('VERSION should return the correct version', function () {
+    it('VERSION should return the jqLite version', function () {
       assert.equal(jqLite.version, '0.1.0', "jqLite.getVersion() provides the incorrect version");
+      assert.isTrue(typeof jqLite.version === 'string', 'jqLite.getVersion() does not return a string');
+    });
+    it('VERSION should have the correct format 0.0.0', function () {
+      const regExp = /^(\d[.]){2}\d$/;
+      expect(regExp.test(jqLite.version));
     });
   });
 
